06567ff211bc1f44d72e52ec29d3df07a5e08635
desaraballabani <desara.ballabani@base-com.al>
2023-10-19 17:15:45 +0200

    Add Product, API Integration

--- src/app/seller-add-product/seller-add-product.component.ts
+++ src/app/seller-add-product/seller-add-product.component.ts
@@ -1,17 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../services/product.service';
+import { product } from '../data-type';
 
 @Component({
   selector: 'app-seller-add-product',
   templateUrl: './seller-add-product.component.html',
   styleUrls: ['./seller-add-product.component.css']
 })
-export class SellerAddProductComponent {
-  constructor() { }
+export class SellerAddProductComponent implements OnInit {
+  addProductMessage: string | undefined;
+  constructor(private product: ProductService) { }
   ngOnInit(): void {
 
   }
-  submit(data: object) {
-    console.warn(data)
+  submit(data: product) {
+    this.product.addProduct(data).subscribe((result) => {
+      console.warn(result)
+      if (result) {
+        this.addProductMessage = "Product is succesfully added"
+      }
+      setTimeout(() => this.addProductMessage = undefined, 3000)
+    })
   }
 
 }
