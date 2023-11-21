2092d4668c2faf724be5ba2fc36d89e27381d147
desaraballabani <desara.ballabani@base-com.al>
2023-11-21 16:33:17 +0100

    Sign Up Form in Angular

---
+++ src/app/user-auth/user-auth.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { SignUp } from '../data-type';
+
+@Component({
+  selector: 'app-user-auth',
+  templateUrl: './user-auth.component.html',
+  styleUrls: ['./user-auth.component.css']
+})
+export class UserAuthComponent implements OnInit {
+  ngOnInit(): void {
+
+  }
+
+  signUp(data: SignUp) {
+    console.warn(data)
+  }
+
+}
